e1e65255a335824e5ece6d46625f0edb0ab8be4e
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-19 10:06:39 +0100

    create societe repo

---
+++ src/main/java/com/iset/service/dao/SocieteRepository.java
@@ -0,0 +1,16 @@
+package com.iset.service.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.iset.service.entity.Societe;
+
+@RepositoryRestResource
+public interface SocieteRepository extends JpaRepository<Societe, Long> {
+	@RestResource(path = "name")
+	List<Societe> findByName(@Param("name") String name);
+}
