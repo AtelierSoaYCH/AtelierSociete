47d9ba28aeec7d6ff678c5006bc406450749c70e
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-19 10:04:14 +0100

    create societe entity

---
+++ src/main/java/com/iset/service/entity/Societe.java
@@ -0,0 +1,24 @@
+package com.iset.service.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Societe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double code;
+}
